f9c91f07232b3ec0442adb1e74a6bcfe2319e2fe
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2022-10-21 00:09:52 +0300

    scraper lunarbaboon.py is worked

--- lunarbaboon/lunarbaboon.py
+++ lunarbaboon/lunarbaboon.py
@@ -1,6 +1,4 @@
 import os
-import time
-
 import requests
 
 from bs4 import BeautifulSoup
@@ -16,7 +14,7 @@ os.makedirs('lunarbaboon_downloads', exist_ok=True)
 
 
 def get_html(url=HOST):
-    """"""
+    """"Get html of page for parsing"""
     sess = requests.Session()
 
     while True:
@@ -24,44 +22,49 @@ def get_html(url=HOST):
         res.raise_for_status()
         soup = BeautifulSoup(res.text, 'lxml')
         comics_urls = get_comics_urls(soup)
+        # print(comics_urls)
         for comic_url in comics_urls:
             file_name = os.path.basename(comic_url).split('?')[0]
             has_comic = _save_comic(comic_url, file_name)
             if not has_comic:
-                print('Done')
                 return
-
-        time.sleep(1)
-        url = get_next_page(soup)
+        try:
+            url = get_next_page(soup)
+        except IndexError:
+            print('Done!')
+            return
 
 
 def get_comics_urls(soup):
-    """"""
+    """Get all URLs from page"""
     comics_urls = []
-    items = soup.select('.body>p>span>img')
+    items = soup.select('.body')
     for item in items:
         if not item:
             continue
         else:
-            image_link = item.get('src')
-            comic_url = HOST + image_link
+            image_link = item.find_all('img')[0].get('src')
+            if image_link.startswith('http://'):
+                comic_url = image_link
+            else:
+                comic_url = HOST + image_link
             comics_urls.append(comic_url)
 
     return comics_urls
 
 
 def get_next_page(soup):
-    """"""
-    next_page = soup.select('.paginationControlNextPageSuffix>a')[0]
-    url = HOST + next_page.get('href')
+    """Get a URL for next page"""
+    url = HOST + soup.select('.paginationControlNextPageSuffix>a')[0].get('href')
     return url
 
 
 def _save_comic(comic_url, file_name, headers=HEADERS):
-    """"""
+    """Get URL of image and save file in base folder"""
     res = requests.get(comic_url, headers)
     res.raise_for_status()
     image_path = os.path.join('lunarbaboon_downloads', file_name)
+    # checking file availability
     if not os.path.isfile(image_path):
         print('Download image... %s' % comic_url)
         image_file = open(image_path, 'wb')
@@ -75,9 +78,14 @@ def _save_comic(comic_url, file_name, headers=HEADERS):
 
 
 def main():
-    """"""
-    # url = 'http://www.lunarbaboon.com/comics/?currentPage=195'
-    get_html(url=HOST)
+    """Start the main process"""
+    try:
+        # this a last page
+        url = 'http://www.lunarbaboon.com/comics/?currentPage=195'
+        get_html(url)
+    except KeyboardInterrupt:
+        print('Forced program termination!')
+        return
 
 
 if __name__ == '__main__':
