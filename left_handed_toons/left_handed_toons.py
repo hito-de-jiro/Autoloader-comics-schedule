ac08171cffbdc9fefae8befec894a2fdeff63655
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2022-10-20 22:57:47 +0300

    scraper left_handed_toons.py is worked

--- left_handed_toons/left_handed_toons.py
+++ left_handed_toons/left_handed_toons.py
@@ -3,11 +3,11 @@ import requests
 
 from bs4 import BeautifulSoup
 
-URL = 'http://www.lefthandedtoons.com/'
+HOST = 'http://www.lefthandedtoons.com/'
 os.makedirs('left_handed_toons_downlods', exist_ok=True)
 
 
-def get_html(url=URL):
+def get_html(url=HOST):
     while True:
         #  завантаженя сторінки
         res = requests.get(url)
@@ -19,31 +19,49 @@ def get_html(url=URL):
             print('Image not found')
         else:
             comic_url = comic_elem[0].get('src')
-
-            res = requests.get(comic_url)
-            res.raise_for_status()
-            image_path = os.path.join('left_handed_toons_downlods', os.path.basename(comic_url))
-
-            if not os.path.isfile(image_path):
-                print('Download image... %s' % comic_url)
-                image_file = open(image_path, 'wb')
-                for chunk in res.iter_content(100_000):
-                    image_file.write(chunk)
-                image_file.close()
-            else:
-                print('Image exist!')
+            has_comic = save_comic(comic_url)
+            if not has_comic:
                 return
-
         try:
-            prev_link = soup.select('.prev>a')[0]
-            url = URL + prev_link.get('href')
+            url = prev_link(soup)
         except IndexError:
             print('Done!')
             break
 
 
+def save_comic(comic_url):
+    """Get URL of image and save file in base folder"""
+    res = requests.get(comic_url)
+    res.raise_for_status()
+    image_path = os.path.join('left_handed_toons_downlods', os.path.basename(comic_url))
+    # checking file availability
+    if not os.path.isfile(image_path):
+        print('Download image... %s' % comic_url)
+        image_file = open(image_path, 'wb')
+        for chunk in res.iter_content(100_000):
+            image_file.write(chunk)
+        image_file.close()
+        return True
+    else:
+        print('No new comics!')
+        return False
+
+
+def prev_link(soup):
+    """Get a URL preview link"""
+    url = HOST + soup.select('.prev>a')[0].get('href')
+    return url
+
+
 def main():
-    get_html(url=URL)
+    """Start the main process"""
+    try:
+        # this a last page
+        # url = 'https://www.lefthandedtoons.com/1/'
+        get_html()
+    except KeyboardInterrupt:
+        print('Forced program termination!')
+        return
 
 
 if __name__ == '__main__':
