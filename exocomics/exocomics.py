0f518dedd078d3a5132d685be13cd9d8a0cdf512
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2022-10-20 22:43:34 +0300

    scraper exocomics.py is worked

--- exocomics/exocomics.py
+++ exocomics/exocomics.py
@@ -3,7 +3,7 @@ import requests
 
 from bs4 import BeautifulSoup
 
-URL = 'https://www.exocomics.com'
+HOST = 'https://www.exocomics.com'
 HEADERS = {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,"
               "*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -11,52 +11,39 @@ HEADERS = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                   "(KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
 }
+os.makedirs('exocomics_download', exist_ok=True)
 
 
-def main(url=URL):
-    """Отримує URL комікса і передає його на завантаження"""
-    flag = True
+def get_html(url=HOST):
+    """"Get html of page for parsing"""
     sess = requests.Session()
-
-    while flag:
+    while True:
         res = sess.get(url, headers=HEADERS)
         res.raise_for_status()
         soup = BeautifulSoup(res.text, 'lxml')
         try:
             item = soup.find('img', class_='image-style-main-comic').get('src')
-            comic_url = URL + item
-            # print(f"Page {comic_url} ...")
+            comic_url = HOST + item
             has_new_comic = _save_comic(comic_url, headers=HEADERS)
             if not has_new_comic:
-                flag = False
-
+                return
         except AttributeError:
             print(f"Page {url} -- Image not found")
             continue
         finally:
             prev_link = _prev_url(soup)
-            if prev_link == '':
-                print('Done!')
-                flag = False
-            else:
-                url = URL + prev_link
-
-
-def _prev_url(soup):
-    try:
-        prev_link = soup.find('a', class_='ir prev').get('href')
-    except AttributeError:
-        prev_link = ''
-    return prev_link
+            url = HOST + prev_link
+        if prev_link == '':
+            print('Done!')
+            return
 
 
 def _save_comic(comic_url, headers=HEADERS):
-    """Перевіряє наявність і завантажує файл"""
+    """Get URL of image and save file in base folder"""
     res = requests.get(comic_url, headers)
     res.raise_for_status()
     image_path = os.path.join('exocomics_download', os.path.basename(comic_url))
     if not os.path.isfile(image_path):  # Перевірка існування файлу.
-        print('You have a new comics')
         print('Download image... %s' % comic_url)
         image_file = open(image_path, 'wb')
         for chunk in res.iter_content(100_000):
@@ -66,6 +53,26 @@ def _save_comic(comic_url, headers=HEADERS):
     else:
         print('No new comics!')
         return False
+
+
+def _prev_url(soup):
+    """Get a URL preview link"""
+    try:
+        prev_link = soup.find('a', class_='ir prev').get('href')
+    except AttributeError:
+        prev_link = ''
+    return prev_link
+
+
+def main():
+    """Start the main process"""
+    try:
+        # this a last page
+        # url = 'https://www.exocomics.com/500/'
+        get_html()
+    except KeyboardInterrupt:
+        print('Forced program termination!')
+        return
 
 
 if __name__ == "__main__":
