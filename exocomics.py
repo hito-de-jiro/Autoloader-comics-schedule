c726df8538b50cf93a5e16731d0b71e19a98c977
Hito <60074087+hito-de-jiro@users.noreply.github.com>
2022-10-26 00:16:21 +0300

    exocomics.py -- add set download time limit and check datetime

--- exocomics.py
+++ exocomics.py
@@ -1,8 +1,10 @@
+import argparse
+import datetime
 import os
 import requests
-import argparse
 
 from bs4 import BeautifulSoup
+from dateutil.parser import parse as parse_date
 
 HOST = 'https://www.exocomics.com'
 HEADERS = {
@@ -12,24 +14,29 @@ HEADERS = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                   "(KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
 }
+DEFAULT_PATH = 'comics_folder/exocomics'
 
 
-def get_html(comics_folder, url=HOST):
+def get_html(comics_folder, date_limit, url=HOST):
     """"Get html of page for parsing"""
     sess = requests.Session()
     while True:
         res = sess.get(url, headers=HEADERS)
         res.raise_for_status()
         soup = BeautifulSoup(res.text, 'lxml')
+
         try:
-            item = soup.find('img', class_='image-style-main-comic').get('src')
-            comic_url = HOST + item
-            has_new_comic = save_comic(comics_folder, comic_url, headers=HEADERS)
-            if not has_new_comic:
+            comic_link = soup.find('img', class_='image-style-main-comic').get('src')
+            comic_url = HOST + comic_link
+            comic_date = soup.find('div', class_='date').getText()
+            # print(comic_date)
+            comic_date = parse_date(comic_date)
+
+            if date_limit and comic_date < date_limit:
+                print(f'Done. Got date limit')
                 return
-        except AttributeError:
-            print(f"Page {url} -- Image not found")
-            continue
+            save_comic(comic_url, comics_folder, comic_date)
+
         finally:
             prev_link = _prev_url(soup)
             url = HOST + prev_link
@@ -38,11 +45,12 @@ def get_html(comics_folder, url=HOST):
             return
 
 
-def save_comic(comics_folder, comic_url, headers=HEADERS):
+def save_comic(comic_url, comics_folder, comic_date, headers=HEADERS):
     """Get URL of image and save file in base folder"""
     res = requests.get(comic_url, headers)
     res.raise_for_status()
-    image_path = os.path.join(comics_folder, os.path.basename(comic_url))
+    comic_name = comic_date.strftime("%Y-%m-%d") + '_' + os.path.basename(comic_url)
+    image_path = os.path.join(comics_folder, comic_name)
     if not os.path.isfile(image_path):  # Перевірка існування файлу.
         print('Download image... %s' % comic_url)
         image_file = open(image_path, 'wb')
@@ -51,7 +59,7 @@ def save_comic(comics_folder, comic_url, headers=HEADERS):
         image_file.close()
         return True
     else:
-        print('No new comics!')
+        print(f'Date: {comic_date.strftime("%Y-%m-%d")} -- No new comics!')
         return False
 
 
@@ -64,7 +72,7 @@ def _prev_url(soup):
     return prev_link
 
 
-def main(comics_folder):
+def main(comics_folder, date_limit):
     """Start the main process"""
     print('Exocomic start')
     print(f'Comics folder is {comics_folder}')
@@ -72,28 +80,37 @@ def main(comics_folder):
     try:
         # this a last page
         # url = 'https://www.exocomics.com/500/'
-        get_html(comics_folder)
+        get_html(comics_folder, date_limit)
     except KeyboardInterrupt:
         print('Forced <exocomic> program termination!')
         return
 
+def valid_date(s):
+    """Datetime validator"""
+    try:
+        return datetime.datetime.strptime(s, '%Y-%m-%d')
+    except ValueError:
+        msg = "not a valid date: {0!r}".format(s)
+        raise argparse.ArgumentTypeError(msg)
 
-def choice_folder() -> str:
+
+def parse_params():
     """Choice output comics folder"""
 
     parser = argparse.ArgumentParser(prog='loader', description='loader comics shit')
     parser.add_argument('--outdir', type=str, default=None, help='Output absolut path')
+    parser.add_argument('--date_limit', type=valid_date,
+                        default=None, help="The Date - format YYYY-MM-DD")
     args = parser.parse_args()
 
-    default_path = 'comics_folder/exocomics'
-    outdir = args.outdir
-    if outdir is None:
-        return default_path
-    elif os.path.isabs(outdir):
-        return outdir
-    else:
+    if args.outdir is None:
+        args.outdir = DEFAULT_PATH
+    elif not os.path.isabs(args.outdir):
         raise ValueError('Path is not absolute')
+
+    return args
 
 
 if __name__ == "__main__":
-    main(comics_folder=choice_folder())
+    params = parse_params()
+    main(comics_folder=params.outdir, date_limit=params.date_limit)
