90f86c2f847d93c6cb9c997f3adef5d9ac4bd180
hito-de-jiro <lollexuss@gmail.com>
2023-11-04 20:00:15 +0200

    feat!: comics_logs.py needs refinement

--- comics_logs.py
+++ comics_logs.py
@@ -1,24 +1,22 @@
-import datetime
 import json
+import os
 
-from pprint import pprint
-from loader import all_files_date as comics_data
-from loader import choice_folder as choice_folder
+from loader import get_last_date as last_data
 from loader import check_new_comics
 
+DESKTOP_PATH = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
 JSON_PATH = 'info_comics.json'
 
 
-def create_log(comic_name: str, last_date):
-    """создать журнал загрузки комиксов. JSON файл - название комикса: дата последнего комикса"""
-
-    comics_path = choice_folder()
-    all_data_time = comics_data(comics_path)
+def create_log(comic_name: str, last_date=last_data):
+    """Create a comic download magazine. JSON file - the name of the comic: the date of the last comic"""
+    comics_path = os.path.join((os.getcwd()), 'comics_folder')
+    # all_data_time = comics_data(comics_path)
     new_comics_date = check_new_comics(comics_path)
     print(new_comics_date)
     data_log = {comic_name: last_date.isoformat()}
 
-    data_time = {k: v.isoformat() for k, v in all_data_time.items()}
+    # data_time = {k: v.isoformat() for k, v in all_data_time.items()}
 
     create_log_json(data_log)
 
@@ -29,7 +27,7 @@ def create_log_json(data_time: dict, path=JSON_PATH):
 
 
 def get_log_json(path=JSON_PATH) -> dict:
-    """получает JSON и возвращает его словарем"""
+    """receives JSON and returns it as a dictionary"""
     with open(path) as f:
         log_data = json.load(f)
     return log_data
@@ -38,14 +36,3 @@ def get_log_json(path=JSON_PATH) -> dict:
 create_log(JSON_PATH)
 
 dates = get_log_json(path=JSON_PATH)
-# for data in dates.values():
-#     print(datetime.datetime.strptime(data, '%Y-%m-%dT%H:%M:%S.%f'))
-# now_time = datetime.datetime.now()
-# y = now_time.isoformat()  # <-- str
-# print(y)
-# print(type(y))
-# x = datetime.datetime.strptime(y, '%Y-%m-%dT%H:%M:%S.%f')  # <-- datetime
-# print(x)
-# print(type(x))
-# a = {print(datetime.datetime.strptime(data, '%Y-%m-%dT%H:%M:%S.%f')) for data in dates.values()}
-# print(a)
